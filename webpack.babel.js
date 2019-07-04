ae73c5e48d3e2805d9e0411035785c0277245e84
RoarkTech <josh@digitalcake.co.nz>
2019-07-04 14:49:00 +0100

    Added OTF files to file-loader

--- webpack.babel.js
+++ webpack.babel.js
@@ -66,7 +66,7 @@ module.exports = {
         }
       },
       {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        test: /\.(woff(2)?|ttf|otf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
         use: [{
           loader: 'file-loader',
           options: {
